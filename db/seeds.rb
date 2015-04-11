d23710a8f0b91ec5558a7bfca3735fe9836da221
Rafael Souza <rafael.ssouza@gmail.com>
2015-04-11 18:37:56 -0300

    database seed for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.connection.execute("TRUNCATE TABLE users")
+
+users = User.create!([
+  { name: "Eduardo", email: "eduardo@thelaborat.org", password: "aaa123", password_confirmation: "aaa123" },
+  { name: "João", email: "joaofranscisconeto@gmail.com", password: "aaa123", password_confirmation: "aaa123" },
+  { name: "Rafael", email: "rafael.ssouza@gmail.com", password: "aaa123", password_confirmation: "aaa123" }
+])
+
+Menu.create([
+  { user_id: users[0].id, name: "Pizza", price: 10.99, price_per_person: 2.99, number_of_people: (2..10) }
+])
