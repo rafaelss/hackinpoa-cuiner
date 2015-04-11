5648133ff8087704ee650566c0805f9d20d04eb4
Rafael Souza <rafael.ssouza@gmail.com>
2015-04-11 15:56:46 -0300

    user creation

--- Gemfile
+++ Gemfile
@@ -5,8 +5,12 @@ ruby "2.2.1"
 gem 'rails', '4.2.1'
 gem 'pg', '0.18.1'
 gem 'puma', '2.11.1'
+gem 'bcrypt', '3.1.10'
+gem 'responders', '2.1.0'
+gem 'active_model_serializers', '0.9.3'
 
 group :development, :test do
   gem 'byebug', '4.0.5'
   gem 'web-console', '2.1.2'
+  gem 'rspec-rails', '3.2.1'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150411183727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",                                           null: false
+    t.string   "email",                                          null: false
+    t.string   "password_digest",                                null: false
+    t.text     "bio"
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.uuid     "public_id",       default: "uuid_generate_v4()"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
