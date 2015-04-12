a5387d5d7f673892e913e2ba539165eea7be4a3b
Rafael Souza <rafael.ssouza@gmail.com>
2015-04-12 02:11:12 -0300

    destroy is necessary to remove objects from the search index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
-User.connection.execute("TRUNCATE TABLE users")
-Menu.connection.execute("TRUNCATE TABLE menus")
+User.all.map(&:destroy)
 
 users = User.create!([
   { name: "Eduardo Costa", email: "eduardo@thelaborat.org", password: "aaa123", password_confirmation: "aaa123" },
@@ -12,3 +11,6 @@ menus = Menu.create!([
   { user_id: users[1].id, name: "Churrasco", price: 45, price_per_person: 10, number_of_people: [5, 20], tags: ["picanha", "costela", "maminha", "vazio"] },
   { user_id: users[2].id, name: "Comida Japonesa", price: 20, price_per_person: 20, number_of_people: [2, 5], tags: ["sushi", "sashimi"] }
 ])
+
+ap Menu.search("churras")
+ap Menu.search("sushi")
