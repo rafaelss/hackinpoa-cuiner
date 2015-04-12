f51de4b206af901b0722382e9b717f7de0207ba2
Rafael Souza <rafael.ssouza@gmail.com>
2015-04-11 22:34:41 -0300

    allow sign in with a different user

--- spec/support/sign_in_helper.rb
+++ spec/support/sign_in_helper.rb
@@ -1,8 +1,6 @@
 module SignInHelper
-  PASSWORD = "aaa123".freeze
-
-  def sign_in
-    user = create_user
-    post authenticate_user_path, email: user.email, password: PASSWORD
+  def sign_in(user = nil)
+    user ||= create_user
+    post authenticate_user_path, email: user.email, password: "aaa123"
   end
 end
